95f26e807d878082761e4cbfb1caecbca5cec6e8
Brad Cypert <Brad.Cypert@gmail.com>
2016-08-21 16:46:28 -0700

    almost there

---
+++ src/skeptic/test.clj
@@ -0,0 +1,34 @@
+(ns skeptic.test)
+
+(def errors {
+             :no-expect "No Expectation Specified. Ensure that your test options includes an :expect."
+             })
+
+(defn- human-to-kebab [s]
+  (clojure.string/replace s #" " "-"))
+
+;(test “update their last name” {
+;      :given   [user (->User “Frank” “Jonson”)]
+;      :when    (setLastName user “Johnson”)
+;      :expect  (= (getLastName user) “Johnson”)})
+
+
+(defmacro test
+  "skeptic.test/test
+   (test-name options)
+   Defines a new test with the given assertions
+   the options map takes up-to 3 key value pairs:
+   :given, :when, :expect
+   :given [optional] - a vector representing a let binding to be used in your test.
+   :when  [optional] - a "
+  [test-name options]
+  (println (:given options))
+  (let [givenf#  (:given options)
+        whenf#   (:when options)
+        expectf# (:expect options)]
+  `(cond
+    (nil?  ~expectf#) (throw (ex-info (:no-expect errors) {:expect ~expectf#}))
+    :default (let ~givenf#
+               ~whenf#
+               (assert ~expectf# (str test-name "failed the given expectation:" ~expectf#)))) 
+    ))
