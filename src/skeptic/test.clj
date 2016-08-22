2f943c9be6bb8fbb2543dc7a0ac2a76cfad673a0
Brad Cypert <Brad.Cypert@gmail.com>
2016-08-21 17:55:16 -0700

    v1.0?

--- src/skeptic/test.clj
+++ src/skeptic/test.clj
@@ -19,16 +19,15 @@
    Defines a new test with the given assertions
    the options map takes up-to 3 key value pairs:
    :given, :when, :expect
-   :given [optional] - a vector representing a let binding to be used in your test.
-   :when  [optional] - a "
+   :given  [optional] - a vector representing a let binding to be used in your test.
+   :when   [optional] - a list representing any transformations that the test should occur.
+   :expect [required] - a list representing any expectations that should occur. These are placed within an assert."
   [test-name options]
-  (println (:given options))
   (let [givenf#  (:given options)
         whenf#   (:when options)
         expectf# (:expect options)]
-  `(cond
-    (nil?  ~expectf#) (throw (ex-info (:no-expect errors) {:expect ~expectf#}))
-    :default (let ~givenf#
+  (cond
+    (nil?  expectf#) (throw (ex-info (:no-expect errors) {:expect expectf#}))
+    :default `(let ~givenf#
                ~whenf#
-               (assert ~expectf# (str test-name "failed the given expectation:" ~expectf#)))) 
-    ))
+               (assert ~expectf# (str ~test-name "failed the given expectation:" ~@expectf#))))))
